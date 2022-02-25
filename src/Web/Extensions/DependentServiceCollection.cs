ca466fea281c81ee3a8f7f5c5a720da149b57d0e
Kareem Oluwatobi <toboibo@yahoo.com>
2022-02-25 15:51:02 -0800

    Startup

---
+++ src/Web/Extensions/DependentServiceCollection.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace Web.Extensions
+{
+    internal static class DependentServiceCollection
+    {
+        internal static IServiceCollection AddCustomServices(this IServiceCollection services)
+        {
+            // Add services to the container.
+            services.AddRazorPages();
+            services.Configure<RouteOptions>(options => {
+                options.AppendTrailingSlash = true;
+                options.LowercaseUrls = true;
+                options.LowercaseQueryStrings = true;
+            });
+
+            services.AddHealthChecks();
+            return services;
+        }
+    }
+}
